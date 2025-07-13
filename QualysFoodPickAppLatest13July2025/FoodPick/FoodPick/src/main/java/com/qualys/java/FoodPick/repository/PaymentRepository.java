b54f5fab65ee0cd2f42c88352dd6e344920d1d08
Siddhesh <siddheshmca@gmail.com>
2025-07-13 21:53:57 +0530

    Repository

--- QualysFoodPickAppLatest13July2025/FoodPick/FoodPick/src/main/java/com/qualys/java/FoodPick/repository/PaymentRepository.java
+++ QualysFoodPickAppLatest13July2025/FoodPick/FoodPick/src/main/java/com/qualys/java/FoodPick/repository/PaymentRepository.java
@@ -1,10 +1,11 @@
 package com.qualys.java.FoodPick.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.qualys.java.FoodPick.entity.Payment;
 
-
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long>  {
 
 }
--- QualysFoodPickAppLatest13July2025/FoodPick/FoodPick/src/main/java/com/qualys/java/FoodPick/repository/RestaurantRepository.java
+++ QualysFoodPickAppLatest13July2025/FoodPick/FoodPick/src/main/java/com/qualys/java/FoodPick/repository/RestaurantRepository.java
@@ -1,10 +1,12 @@
 package com.qualys.java.FoodPick.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.yaml.snakeyaml.events.Event.ID;
 
 import com.qualys.java.FoodPick.entity.Restaurant;
 
+@Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, ID> {
 
 }
