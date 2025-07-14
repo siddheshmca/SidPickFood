8cb80f08b6078b95656ffe5f08a7c88852cf51ca
Siddhesh <siddheshmca@gmail.com>
2025-07-14 23:58:51 +0530

    Updated DDL and DML Script

--- QualysFoodPickAppLatest13July2025/FoodPick/FoodPick/src/main/resources/customerDDL.sql
+++ QualysFoodPickAppLatest13July2025/FoodPick/FoodPick/src/main/resources/customerDDL.sql
@@ -14,3 +14,33 @@ CREATE TABLE restaurant (
     rest_phone_no BIGINT
 );
 
+CREATE TABLE cust_addr (
+    cust_addr_id INT PRIMARY KEY,
+    cust_id INT UNIQUE,
+    cust_flat_num INT,
+    cust_floor_num INT,
+    cust_wing VARCHAR(10),
+    cust_bldg_name VARCHAR(100),
+    cust_society_name VARCHAR(100),
+    cust_street_name VARCHAR(100),
+    cust_area VARCHAR(100),
+    cust_city VARCHAR(50),
+    cust_state VARCHAR(50),
+    cust_pin_code INT,
+    CONSTRAINT fk_cust_addr_customer
+      FOREIGN KEY (cust_id)
+      REFERENCES customer(cust_id)
+);
+
+CREATE TABLE rest_addr (
+    rest_addr_id INT PRIMARY KEY,
+    rest_id INT,
+    rest_shop_no INT,
+    rest_street_name VARCHAR(100),
+    rest_floor_num INT,
+    rest_area VARCHAR(100),
+    rest_city VARCHAR(100),
+    rest_state VARCHAR(100),
+    rest_pin_code INT,
+    FOREIGN KEY (rest_id) REFERENCES restaurant(rest_id)
+);
--- QualysFoodPickAppLatest13July2025/FoodPick/FoodPick/src/main/resources/customerDML.sql
+++ QualysFoodPickAppLatest13July2025/FoodPick/FoodPick/src/main/resources/customerDML.sql
@@ -18,3 +18,36 @@ INSERT INTO restaurant (rest_id, rest_name, rest_location, rest_category, rest_m
 (200004, 'Swad Amristsar', 'Juhu Mumbai 400054','Indian','Veg, Non-Veg', 9476594765),
 (200005, 'World of Vegan', 'Nariman Point Mumbai 400032','American','Vegan', 4567845678),
 (200006, 'Bombay Canteen', 'Lower Parel Mumbai 400076','Indian','Veg, Non-Veg', 9080706050);
+
+INSERT INTO cust_addr (
+    cust_addr_id,
+    cust_id,
+    cust_flat_num,
+    cust_floor_num,
+    cust_wing,
+    cust_bldg_name,
+    cust_society_name,
+    cust_street_name,
+    cust_area,
+    cust_city,
+    cust_state,
+    cust_pin_code
+) VALUES
+(150001, 100001, 4509, 45, 'A', 'Blue Heights', 'Blue Heights', 'Worli Seaface Rd', 'Worli', 'Mumbai', 'Maharashtra', 400024),
+(150002, 100002, 4518, 45, 'B', 'Blue Heights', 'Blue Heights', 'Worli Seaface Rd', 'Worli', 'Mumbai', 'Maharashtra', 400024),
+(150003, 100003, 4206, 42, 'C', 'Blue Heights', 'Blue Heights', 'Worli Seaface Rd', 'Worli', 'Mumbai', 'Maharashtra', 400024),
+(150004, 100004, 147, 1, NULL, 'Tendulkar Villa', NULL, 'Jogger''s Park Rd', 'Bandra', 'Mumbai', 'Maharashtra', 400030),
+(150005, 100005, 3109, 31, 'D', 'Seventh Heaven', 'Paradise Society', 'Madh Island Rd', 'Juhu', 'Mumbai', 'Maharashtra', 400054),
+(150006, 100006, 3505, 35, 'E', 'Seventh Heaven', 'Paradise Society', 'Madh Island Rd', 'Juhu', 'Mumbai', 'Maharashtra', 400054),
+(150007, 100007, 5604, 56, 'F', '8 PM Bldg', 'King''s World Society', 'Stock Exchange Rd', 'Nariman Point', 'Mumbai', 'Maharashtra', 400032),
+(150008, 100008, 235, 1, NULL, 'Patil Villa', NULL, 'S.Keer Rd', 'Dadar', 'Mumbai', 'Maharashtra', 400028),
+(150009, 100009, 1304, 13, 'A', 'Matoshree Ankur', NULL, 'Ganesh Peth Rd', 'Dadar', 'Mumbai', 'Maharashtra', 400028),
+(150010, 100010, 2507, 25, 'B', 'Kohinoor Square', 'Kohinoor Square Residential Society', 'N.C.Kelkar Rd', 'Dadar', 'Mumbai', 'Maharashtra', 400028);
+
+INSERT INTO rest_addr (rest_addr_id, rest_id, rest_shop_no, rest_street_name, rest_floor_num, rest_area, rest_city, rest_state, rest_pin_code) VALUES
+(170001, 200001, 12, '1st Cross Rd', 1, 'Worli', 'Mumbai', 'Maharashtra', 400024),
+(170002, 200002, 18, '16th Pali Hill Rd', 2, 'Bandra', 'Mumbai', 'Maharashtra', 400030),
+(170003, 200003, 25, 'Shivaji Park Rd', 1, 'Dadar', 'Mumbai', 'Maharashtra', 400028),
+(170004, 200004, 30, '100 Ft Rd', 1, 'Juhu', 'Mumbai', 'Maharashtra', 400054),
+(170005, 200005, 5, 'Air India Office Rd', 2, 'Nariman Point', 'Mumbai', 'Maharashtra', 400032),
+(170006, 200006, 15, 'Pheonix Market City', 2, 'Lower Parel', 'Mumbai', 'Maharashtra', 400076);
