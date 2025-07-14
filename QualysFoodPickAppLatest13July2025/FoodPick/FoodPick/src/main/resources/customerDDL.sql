de05e989b1d1e3dfd2b4890110b6c4e67028fa90
Siddhesh <siddheshmca@gmail.com>
2025-07-14 15:24:58 +0530

    DML and DDL Scripts

---
+++ QualysFoodPickAppLatest13July2025/FoodPick/FoodPick/src/main/resources/customerDDL.sql
@@ -0,0 +1,6 @@
+CREATE TABLE customer (
+    cust_id INT PRIMARY KEY,
+    cust_Name VARCHAR(100),
+    cust_Addr VARCHAR(255),
+    cust_Phone_No BIGINT
+);
---
+++ QualysFoodPickAppLatest13July2025/FoodPick/FoodPick/src/main/resources/customerDML.sql
@@ -0,0 +1,11 @@
+INSERT INTO customer (cust_id, cust_Name, cust_Addr, cust_Phone_No) VALUES 
+(100001, 'Virat Kohli', 'Worli Mumbai 400024',6578965789),
+(100002, 'Rohit Sharma', 'Worli Mumbai 400024',7865478654),
+(100003, 'Shreyas Iyer', 'Worli Mumbai 400024',5467154671),
+(100004, 'Arjun Tendulkar', 'Bandra Mumbai 400030',9432694326),
+(100005, 'Sunil Gavaskar', 'Juhu Mumbai 400054',6785467854),
+(100006, 'Dilip Vengsarkar', 'Lower Parel Mumbai 400076',4897648976),
+(100007, 'Ravi Shastri', 'Nariman Point Mumbai 400032',8546785467),
+(100008, 'Sandeep Patil', 'Dadar Mumbai 400028',9345693456),
+(100009, 'Pravin Amre', 'Dadar Mumbai 400028',8342183421),
+(100010, 'Ajinkya Rahane', 'Dadar Mumbai 400028',9346793467);
